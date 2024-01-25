afe093742068faedf88ee55b7b0a16311ff37a5c
AhmadOthman <trollingosman101@outlook.com>
2024-01-26 00:44:39 +0200

    -  refactor(gameLogic.js): remove unused functions and comments -  feat(gameLogic.js): add randomAi function to generate random move for AI player

--- src/Components/gameLogic.js
+++ src/Components/gameLogic.js
@@ -1,232 +1,3 @@
-/**
- * Checks if a move is available on a given board.
- * @param {Array} board - The current state of the board.
- * @param {number} move - The move to check availability for.
- * @returns {boolean}  True if the move is available, false otherwise.
- */
-// export const isAvailable = (board, move) => !board[move];
-
-/**
- * Checks if the given array contains a winning combination.
- * @param {Array} array The array to check for winning combinations.
- * @returns {boolean} True if a winning combination is found, false otherwise.
- */
-export const checkCombo = (array) => {
-    return WINNING_COMBOS.some((combo) => {
-        return combo.every((item) => array.includes(item)) && combo.length >= 3;
-    });
-};
-
-/**
- * Checks if there is a winner in the current game state and updates the game
- * state accordingly.
- * @param {object} gameState - The current game state object.
- * @param {function} setGameState - The function to update the game state.
- * @returns {boolean}  Returns true if there is a winner, false otherwise.
- */
-export const checkWinner = (gameState, setGameState) => {
-    if (gameState.winner) return true;
-
-    const XResults = checkCombo(gameState.XLocations);
-
-    if (XResults) {
-        setGameState((prev) => {
-            return {
-                ...prev,
-                XScore: prev.XScore + 1,
-                winner: "X",
-                gameEnded: true,
-            };
-        });
-        return true;
-    }
-
-    const OResults = checkCombo(gameState.OLocations);
-
-    if (OResults) {
-        setGameState((prev) => {
-            return {
-                ...prev,
-                OScore: prev.OScore + 1,
-                winner: "O",
-                gameEnded: true,
-            };
-        });
-        return true;
-    }
-
-    return false;
-};
-
-/**
- * Checks if the game has ended in a draw and updates the game state
- * accordingly.
- * @param {object} gameState - The current state of the game.
- * @param {function} setGameState - The function to update the game state.
- * @returns {boolean} True if the game has ended in a draw, false otherwise.
- */
-export const checkDraw = (gameState, setGameState) => {
-    if (gameState.winner || gameState.isDraw) return;
-    const isBoardFull =
-        gameState.XLocations.length + gameState.OLocations.length === 9;
-    const noWinner = !gameState.winner;
-    if (isBoardFull && noWinner) {
-        setGameState((prev) => {
-            return {
-                ...prev,
-                OScore: prev.OScore + 0.5,
-                XScore: prev.XScore + 0.5,
-                isDraw: true,
-                gameEnded: true,
-            };
-        });
-        return true;
-    }
-    return false;
-};
-
-/**
- * Generates a random move for the AI player in a tic-tac-toe game.
- * @param {object} gameState - The current state of the game.
- * @param {function} setGameState - A function to update the game state.
- */
-export const randomAi = (gameState, setGameState) => {
-    const isBoardFull =
-        gameState.XLocations.length + gameState.OLocations.length === 9;
-
-    if (gameState.gameEnded || isBoardFull || gameState.winner) return;
-
-    let move;
-    do {
-        move = Math.floor(Math.random() * 9);
-    } while (!isAvailable(gameState.board, move));
-
-    const newBoard = [...gameState.board];
-    newBoard[move] = "O";
-
-    const newGameState = {
-        ...gameState,
-        XTurn: !gameState.XTurn,
-        board: newBoard,
-        OLocations: [...gameState.OLocations, move],
-    };
-
-    setGameState(newGameState);
-
-    // Check for the end of the game after making a move
-    if (
-        checkWinner(newGameState, setGameState) ||
-        checkDraw(newGameState, setGameState)
-    )
-        return;
-};
-
-/**
- * Resets the game state to its initial values.
- * @param {function} setGameState - The function to update the game state.
- */
-export const Reset = (setGameState) => {
-    setGameState((prev) => {
-        return {
-            ...prev,
-            XTurn: true,
-            XLocations: [],
-            OLocations: [],
-            winner: false,
-            isDraw: false,
-            gameEnded: false,
-            board: Array(9).fill(false),
-        };
-    });
-};
-
-export const AdvancedAi = (gameState, setGameState) => {
-    //
-};
-
-/**
- * Checks if there is a winner in the current game state and updates the game
- * state accordingly.
- * @param {object} gameState - The current game state object.
- * @param {function} setGameState - The function to update the game state.
- * @returns {boolean}  Returns true if there is a winner, false otherwise.
- */
-// export const checkFakeWinner = (gameState) => {
-//     if (gameState.winner) return gameState.winner;
-
-//     const XResults = checkCombo(gameState.XLocations);
-//     if (XResults) return "X";
-
-//     const OResults = checkCombo(gameState.OLocations);
-//     if (OResults) return "O";
-
-//     return false;
-// };
-
-/**
- * Checks if the game has ended in a draw and updates the game state
- * accordingly.
- * @param {object} gameState - The current state of the game.
- * @param {function} setGameState - The function to update the game state.
- * @returns {boolean} True if the game has ended in a draw, false otherwise.
- */
-// export const checkFakeDraw = (gameState) => {
-//     if (gameState.winner || gameState.isDraw) return false;
-
-//     const isBoardFull =
-//         gameState.XLocations.length + gameState.OLocations.length === 9;
-
-//     const noWinner = !gameState.winner;
-
-//     if (isBoardFull && noWinner) return true;
-
-//     return false;
-// };
-
-// export const miniMax = (gameState, board, isMaximizing) => {
-//     // Check if the computer has won
-//     if (checkFakeWinner(gameState) == "X") return 100;
-//     // Check if the player has won
-//     else if (checkFakeWinner(gameState) == "O") return -100;
-//     // Check if the game is a draw
-//     else if (checkFakeDraw(gameState)) return 0;
-
-//     //If it's the computer's turn (maximizing), find the best move
-//     if (isMaximizing) {
-//         let bestScore = NEGA_INF;
-//         let newBoard = [...board];
-//         //Loop through each position on the board
-//         newBoard.map((cell, index) => {
-//             if (!cell) {
-//                 newBoard[index] = "O";
-//                 //Call minimax to find the score for the current move
-//                 let score = miniMax(newBoard, false);
-//                 newBoard[index] = false;
-//                 //Update the best move if the current score is better
-//                 if (score > bestScore) bestScore = score;
-//             }
-//         });
-//         // Return the best score for the maximizing player
-//         return bestScore;
-//     } else {
-//         let bestScore = POS_INF;
-//         let newBoard = [...board];
-//         //Loop through each position on the board
-//         newBoard.map((cell, index) => {
-//             if (!cell) {
-//                 newBoard[index] = "X";
-//                 //Call minimax to find the score for the current move
-//                 let score = miniMax(newBoard, true);
-//                 newBoard[index] = false;
-//                 //Update the best move if the current score is better
-//                 if (score < bestScore) bestScore = score;
-//             }
-//         });
-//         // Return the best score for the maximizing player
-//         return bestScore;
-//     }
-// };
-
 // Winning combinations - a constant because it doesn't change
 export const WINNING_COMBOS = [
     [0, 1, 2], // Top row
@@ -242,10 +13,8 @@ export const WINNING_COMBOS = [
 const POS_INF = Number.POSITIVE_INFINITY; // positive infinity
 const NEGA_INF = Number.NEGATIVE_INFINITY; //negative infinity
 
-export const isAvailable = (board, move) => !board[move];
-
 export const makeMove = (board, move, side) => {
-    if (!isWinner(board, side) && !isDraw(board) && !board[move]) {
+    if (!board[move]) {
         let newBoard = [...board];
         newBoard[move] = side;
         return newBoard;
@@ -257,14 +26,16 @@ export const isWinner = (board, side) => {
     for (let i = 0; i < WINNING_COMBOS.length; i++) {
         const [a, b, c] = WINNING_COMBOS[i];
         if (board[a] === side && board[b] === side && board[c] === side) {
-            return side;
+            return true;
         }
     }
     return false;
 };
 
 export const isDraw = (board) => {
-    return board.every((cell) => cell);
+    const xWon = isWinner(board, "X");
+    const oWon = isWinner(board, "O");
+    return !xWon && !oWon && board.every((cell) => cell);
 };
 
 export const playerMove = (board, move, side = "X") => {
@@ -325,4 +96,20 @@ export const minimax = (board, side, isMaximizing) => {
 
         return bestScore;
     }
+};
+
+export const randomAi = (board) => {
+    // If the game has ended, don't play anymore
+    // if (gameEnded) return;
+    const isBoardFull = board.every((cell) => cell);
+    if (isBoardFull) return;
+
+    let move;
+    do {
+        move = Math.floor(Math.random() * 9);
+    } while (board[move]);
+
+    const newBoard = [...board];
+    newBoard[move] = "O";
+    return newBoard;
 };
